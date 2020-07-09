2555c321f1276aa69af5aa521b3aeb510ef4155a
alex2chivas <alex2chivas@hotmail.com>
2020-07-09 15:41:21 -0700

    added deploy

---
+++ dist/precache-manifest.e8dbd5e74908169130ca98d31762556e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efc82693fbd3b267f4de991546d5567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2b5d5beac9703762ce5",
+    "url": "/static/css/main.598095a3.chunk.css"
+  },
+  {
+    "revision": "e1142c4ebea6ebfc1771",
+    "url": "/static/js/2.56e8fef8.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.56e8fef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b5d5beac9703762ce5",
+    "url": "/static/js/main.23082bb5.chunk.js"
+  },
+  {
+    "revision": "acdf868312f78a97cdd0",
+    "url": "/static/js/runtime-main.794c9aa6.js"
+  }
+]);
